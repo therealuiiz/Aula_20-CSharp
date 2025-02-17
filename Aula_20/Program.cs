4b7c1520841d994a097072556b416aa7738e2e0c
therealuiiz <fluiz4216@gmail.com>
2025-02-17 10:46:52 -0300

    Add project files.

---
+++ Aula_20/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Aula_20;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Read();
+
+    }
+}
+
+
+class Carro
+{
+    public void DigaSeuNome()
+    {
+        Console.WriteLine("Eu sou um carro");
+    }
+}
+
+class Aviao
+{
+    public void DigaSeuNome()
+    { 
+        Console.WriteLine("Eu sou um carro");
+    }
+       
+}
